7c1b31101315c048ae3afe419769e7514ad36b08
Thanos Giannopoulos <thanosgn@gmail.com>
2016-05-19 13:18:46 +0300

    Added file monitoring for attack

--- attack.sh
+++ attack.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 
+# for i in `seq 1 $1`;
+# do
+# 	./rainbow_attack $i &
+# done
+# wait
+
+HASH_FILE='/home/thanos/repositories/rainbow-attack/hash_file.txt'
 for i in `seq 1 $1`;
 do
-	./rainbow_attack $i &
+	./rainbow_attack $i < ${HASH_FILE} &
 done
-wait
+while true
+do
+	echo "Type Hash:"
+	read hash
+	echo "$hash" >> ${HASH_FILE} 
 
-# #!/bin/sh
-
-# while true
-# do
-# 	echo "Type Hash:"
-# 	read hash
-
-# 	for i in `seq 1 $1`;
-# 	do
-# 		./rainbow_attack $i $hash &
-# 	done
-# 	# wait
-# done
-# # wait
+	# wait
+done
+# wait
--- rainbow_attack.cpp
+++ rainbow_attack.cpp
@@ -1,5 +1,6 @@
 #include "rainbow_table.hpp"
-
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 static const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@";
@@ -18,19 +19,42 @@ int main (int argc, char* argv[]){
 		file >> hash;
 		hashMap.insert(pair<string, string>(hash,pass));
 	}
-	char hash_filename[50] = "hashes.txt";
+	// char hash_filename[50] = "hashes.txt";
 	// string hash2(argv[2]);
 	// cout << "Searching: " << hash2 << " in table " << table_number << endl;
 	// searchHash(hashMap, hash2);
-	string hash2, line;
-	ifstream hash_file(hash_filename);
-	while(!hash_file.eof()){
-		hash_file >> hash2;
-		cout << "Searching: " << hash2 << " in table " << table_number << endl;
-		searchHash(hashMap, hash2);
-		// if(searchHash(hashMap, hash2) == true)
-			// return 0;
+	char str[65];
+	int ch;
+	while(true){
+		if((cin.getline(str,65))){
+			 // cout << (char)ch;
+			string hash2(str);
+			cout << "Searching: " << hash2 << " in table " << table_number << endl;
+			searchHash(hashMap, hash2);
+		} else {
+			usleep(500000);
+			cin.clear();
+		}
 	}
+
+
+	// while(true){
+	// 	string hash2;
+	// 	cout << "Type Hash:" << endl;
+	// 	cin >> hash2;
+	// 	searchHash(hashMap, hash2);
+	// }
+
+
+	// string hash2, line;
+	// ifstream hash_file(hash_filename);
+	// while(!hash_file.eof()){
+	// 	hash_file >> hash2;
+	// 	cout << "Searching: " << hash2 << " in table " << table_number << endl;
+	// 	searchHash(hashMap, hash2);
+	// 	// if(searchHash(hashMap, hash2) == true)
+	// 		// return 0;
+	// }
 	// for (int i = 0; i < 100; ++i)
 	// {
 	// 	char* out = new char[16];
